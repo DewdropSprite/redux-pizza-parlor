91418044c0889feb224c2a16338e257693667ae7
Tiffany House-Jimenez <tiffanyhousejimenez@gmail.com>
2024-01-18 11:18:10 -0600

    render pizza feature

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import RenderPizza from '../RenderPizza/RenderPizza';
 
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
+      
+      <RenderPizza />
   
     </div>
   );
