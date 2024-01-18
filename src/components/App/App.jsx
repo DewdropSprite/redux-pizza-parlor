f17802de227907d166077d03b6c9181739333be3
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2024-01-18 11:18:30 -0600

    Got the toggle to work, and added hard coded pizza in

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import HandleAdd from '../ToggleButton/ToggleButton.';
 
 function App() {
+
+  
+
+  // const dispatch = useDispatch()
+  // //this is the GET request to pull the pizzas from the pizza table
+  // const fetchThePizzas = () => {
+  //   console.log("Inside fetchThePizzas");
+  //   axios.get('/api/pizza')
+  //   .then(response => {
+  //     console.log("Pizza List:", response.data);
+  //     dispatch({type: 'SET_PIZZAS', payload: response.data})
+  //   }).catch(error => {
+  //     console.error(error)
+  //     alert('Welp somehting went wrong in fetchThePizzas')
+  //   })
+
+  // }
+
 
   return (
     <div className='App'>
@@ -12,6 +33,12 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
+      <div className='pizzaItem'>
+        <h3>Pizza Title</h3>
+        <p>This is how good the pizza is!</p>
+        <p>Total:</p>
+        <HandleAdd />
+      </div>
   
     </div>
   );
