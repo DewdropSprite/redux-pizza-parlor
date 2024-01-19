9cd58b64616b8ca4155d3f17c02b2eda4f2f73b1
Dustin Hagstrom <dustnhgstrm@gmail.com>
2024-01-19 11:58:52 -0600

    implemented the remove item from shopping cart feature.

--- src/components/PizzaComponent/PizzaComponent.jsx
+++ src/components/PizzaComponent/PizzaComponent.jsx
@@ -9,11 +9,12 @@ export default function PizzaComponent({ pizza }) {
     const dispatch = useDispatch();
     const cart = useSelector((state) => state.itemsInCart);
 
+    // console.log("our shopping cart:", cart);
     const addPizzaToCart = (pizza) => {
         // add the pizza to the cart
         dispatch({
             type: "ADD_PIZZA_TO_CART",
-            payload: [...cart, pizza]
+            payload: [...cart, pizza],
         });
 
         //set the boolean toggle to opp
@@ -21,22 +22,14 @@ export default function PizzaComponent({ pizza }) {
     };
 
     const removePizzaFromCart = (pizza) => {
-        //take the total and subtract the price of the current (pizza.price)
+        let filteredCart = cart.filter((pizzaObj) => pizzaObj.id !== pizza.id);
+        console.log("filtered cart:", filteredCart);
 
-        // The idea is to take the list the represent the pizzas in the cart,
-        // grab that total price from state, then we can subtract the pizza.price
-        // for the pizza that this button was clicked on, then dispatch new total
-        // and then flip the boolean
-
-        //! Below is code that is going to be used, commented for now
-        // let newTotal = pizzaList.reduce((accumulator, currentValue) => {
-        //     return accumulator - currentValue;
-        // }, initialValue = 0)
-
-        // dispatch({
-        //     type: "SUBTRACT_PIZZA_FROM_CART",
-        //     payload: total
-        // }
+        // remove the pizza to the cart
+        dispatch({
+            type: "SUBTRACT_PIZZA_FROM_CART",
+            payload: filteredCart,
+        });
 
         //set the boolean toggle to opp
         setToggle(!toggle);
